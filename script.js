760ca8d306d8a95b9f4b6ae73eb01f1b727e7784
shivamkumaryadavaug <shivamkumaryadavaug11@gmail.com>
2026-03-28 10:27:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMsg(){
+    alert("Welcome to Madan Mohan Malviya 🚀");
+}
+
+function contact(){
+    alert("Message feature coming soon!");
+}
+
+function toggleMenu(){
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
